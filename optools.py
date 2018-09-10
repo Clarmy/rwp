a193b48b357b6d2c7b202a90a8ffba7e89d4f9e5
Clarmy Lee <clarmylee92510@gmail.com>
2018-09-10 10:37:57 +0800

    增加了drop_duplicate_station函数以解决同一标准时间轴上站点重复问题

--- optools.py
+++ optools.py
@@ -27,7 +27,55 @@ def timing(func):
     return wrapper
 
 
-@timing
+def abstr_time(file_name, level='full'):
+    '''从字符串提取时间'''
+    if level == 'hour':
+        result = file_name.split('_')[4][:10]
+    elif level == 'minute':
+        result = file_name.split('_')[4][:12]
+    elif level == 'full':
+        result = file_name.split('_')[4]
+
+    return result
+
+
+def drop_duplicate_station(old_res_dict):
+    '''剔除标准时间轴下重复站点的文件源'''
+    from collections import defaultdict
+
+    def get_station_id(file_name):
+        '''根据文件名提取站点号'''
+        station_id = file_name.split('_')[3]
+        return station_id
+
+    new_res_dict = defaultdict(list)
+    for std_time in old_res_dict:
+        unique_station = set([])
+        for file_name in old_res_dict[std_time]:
+            station_id = get_station_id(file_name)
+            if station_id not in unique_station:
+                unique_station.add(station_id)
+                new_res_dict[std_time].append(file_name)
+            else:
+                pass
+
+    return new_res_dict
+
+
+def gather_res(file_name_lst):
+    '''收集文件源（文件名）'''
+    std_index = standard_time_index()
+    res_dict = {sti: [] for sti in std_index}
+    for key in res_dict:
+        for file_name in file_name_lst:
+            res_timestr = abstr_time(file_name, level='minute')
+            match_timestr = match_standard(res_timestr)
+            if match_timestr == key:
+                res_dict[key].append(file_name)
+
+    return res_dict
+
+
 def standard_time_index():
     '''建立逐6分钟标准时间索引'''
     now = dt.now()
@@ -47,7 +95,6 @@ def standard_time_index():
     return tuple(stdt_index)
 
 
-@timing
 def match_standard(timestr):
     '''（规定格式的）任意时间字符串向标准时间索引的匹配
 
@@ -76,33 +123,6 @@ def match_standard(timestr):
     min_index = min(delt)[1]
 
     return sdt_index[min_index]
-
-
-@timing
-def abstr_time(file_name, level='full'):
-    '''从字符串提取时间'''
-    if level == 'hour':
-        result = file_name.split('_')[4][:10]
-    elif level == 'minute':
-        result = file_name.split('_')[4][:12]
-    elif level == 'full':
-        result = file_name.split('_')[4]
-
-    return result
-
-@timing
-def gather_res(file_name_lst):
-    '''收集文件源'''
-    std_index = standard_time_index()
-    res_pool = {sti: [] for sti in std_index}
-    for key in res_pool:
-        for file_name in file_name_lst:
-            res_timestr = abstr_time(file_name, level='minute')
-            match_timestr = match_standard(res_timestr)
-            if match_timestr == key:
-                res_pool[key].append(file_name)
-
-    return res_pool
 
 
 def main():
