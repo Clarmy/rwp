30782f2055019f2b9b85eefe4e540c7bd85fa010
Clarmy Lee <clarmylee92510@gmail.com>
2018-10-30 16:39:08 +0800

    将save_as_nc函数从makegrid.py转移到了wprio.py

--- wprio.py
+++ wprio.py
@@ -14,6 +14,8 @@ python = 3.6
 '''
 import json as js
 import pandas as pd
+import netCDF4 as nc
+import datetime
 import pdb
 
 
@@ -387,6 +389,83 @@ def save_as_json(data, path_fn, mod='single'):
 
     with open(path_fn, 'w') as fileobj:
         fileobj.write(result_js)
+
+def save_as_nc(data_dict, attr_dict, savepath):
+    '''将数据字典和属性字典融合保存为netCDF4文件
+
+    输入参数
+    -------
+    data_dict : `dict`
+        数据字典，其中必须包括('lon','lat','time')三个辅助变量和至少一个数据变量，如果数据是
+          三维数组，则辅助变量还需要包含('level'), 数据变量内须为('time','lat','lon')或
+          ('time','level','lat','lon')格式数组，类型须为numpy.ndarray。
+
+    attr_dict : `dict`
+        属性字典，双层嵌套型字典，顶层键为'lon','lat','time','level'等变量名，其对应值为该
+          变量的属性字典，为保险起见，避免不必要的异常，最好属性字典的键和值都以str的格式存储。
+          注意：attr_dict的顶层键须与data_dict的键完全一致。
+
+    savepath : `str`
+        输出nc文件保存的完整路径, 须包含文件名及后缀。例如'./output/data.nc'
+
+    返回值
+    -----
+    `bool` : 是否处理成功的标识，若顺利完成，返回True，否则返回False
+
+    示例
+    ----
+    暂无
+
+    '''
+    # 判断数据是三维还是二维
+    dim_num = 3
+    try:
+        data_dict['level']
+    except KeyError:
+        dim_num = 2
+
+    copyright = 'This netCDF4 dataset is parsed, processed and packaged by '\
+        'Beijing Presky Inc., contact us please visit : http://www.cnpresky.com'
+
+    src_keys = set(data_dict.keys())
+    src_keys.remove('lon')
+    src_keys.remove('lat')
+    src_keys.remove('time')
+    if dim_num == 3:
+        src_keys.remove('level')
+
+    src_lon = data_dict['lon']
+    src_lat = data_dict['lat']
+    if dim_num == 3:
+        src_level = data_dict['level']
+
+    with nc.Dataset(savepath, 'w') as file_obj:
+
+        file_obj.createDimension('lon', len(src_lon))
+        file_obj.createDimension('lat', len(src_lat))
+        file_obj.createDimension('time', None)
+        if dim_num == 3:
+            file_obj.createDimension('level', len(src_level))
+
+        opt_data = {}
+        opt_data['lat'] = file_obj.createVariable('lat', float, ('lat',))
+        opt_data['lon'] = file_obj.createVariable('lon', float, ('lon',))
+        opt_data['level'] = file_obj.createVariable('level', float, ('level',))
+        opt_data['time'] = file_obj.createVariable('time', float, ('time',))
+
+        for key in src_keys:
+            opt_data[key] = file_obj.createVariable(key, float,
+                                                    ('level', 'lat', 'lon'))
+
+        for key in data_dict:
+            opt_data[key][:] = data_dict[key]
+            try:
+                opt_data[key].setncatts(attr_dict[key])
+            except KeyError:
+                pass
+            opt_data[key].copyright = copyright
+
+    return True
 
 
 if __name__ == '__main__':
