c90325a1ed9d673f47e7ee8b0fa27cd17a0a675f
Clarmy Lee <clarmylee92510@gmail.com>
2018-11-08 11:03:27 +0800

    风场组网的业务化脚本

---
+++ opr/optmg.py
@@ -0,0 +1,91 @@
+# coding : utf-8
+'''
+--------------------------------------------------------------------
+项目名：rwp
+模块名：opr.optmg
+该模块为业务化插值拼图程序
+--------------------------------------------------------------------
+python = 3.6
+--------------------------------------------------------------------
+'''
+import sys
+sys.path.append('..')
+
+import os
+import time
+import json as js
+
+from optools import check_dir, standard_time_index
+from optools import init_preset, save_preset, load_preset
+from algom.makegrid import full_interp
+
+# 加载配置文件
+with open('../config.json') as f:
+    config = js.load(f)
+
+# 判断测试模式还是业务模式
+try:
+    test_flag = sys.argv[1]
+except IndexError:
+    LOG_PATH = config['mkgrd']['oper']['log_path']
+    SAVE_PATH = config['mkgrd']['oper']['save_path']
+    PRESET_PATH = config['mkgrd']['oper']['preset_path']
+else:
+    if test_flag == 'test':
+        LOG_PATH = config['mkgrd']['test']['log_path']
+        SAVE_PATH = config['mkgrd']['test']['save_path']
+        PRESET_PATH = config['mkgrd']['test']['preset_path']
+    else:
+        raise ValueError('Unkown flag')
+
+# 检查创建目录
+check_dir(LOG_PATH)
+check_dir(PRESET_PATH)
+check_dir(SAVE_PATH)
+
+# 配置日志信息
+import log
+logger = log.setup_custom_logger(LOG_PATH+'wprd','root')
+
+
+def main(rootpath, outpath):
+    # previous_folds = set(os.listdir(rootpath))
+    STD_INDEX = standard_time_index()
+    STD_FILENAMES = [ntime + '.json' for ntime in STD_INDEX]
+
+    while True:
+        folds = os.listdir(rootpath)
+        folds.sort()
+        fold = folds[-1]
+
+        # print('last fold: {}'.format(fold))
+        # logger.info('last fold: {}'.format(fold))
+
+        foldpath = rootpath+fold+'/'
+        files = os.listdir(foldpath)
+        while True:
+            if STD_FILENAMES[0] in files:
+                print('dir {0} finds new'\
+                      ' file:{1}'.format(fold,STD_FILENAMES[0]))
+                logger.info('dir {0} finds new'\
+                      ' file: {1}'.format(fold,STD_FILENAMES[0]))
+                savepath = outpath + fold + '/'
+                check_dir(savepath)
+                savepfn = savepath + \
+                            STD_FILENAMES[0].split('.')[0] + '.nc'
+                full_interp(foldpath + STD_FILENAMES[0], savepath=savepfn)
+                print('making grid from {0} has been finished.'.format(STD_FILENAMES[0]))
+                logger.info('making grid from {0} has been finished.'.format(STD_FILENAMES[0]))
+                STD_FILENAMES.pop(0)
+            else:
+                break
+
+        print('dir {0} has no new file'.format(fold))
+        logger.info('dir {0} has no new file'.format(fold))
+        time.sleep(10)
+        # previous_folds = now_folds
+
+
+if __name__ == '__main__':
+    ROOT_PATH = config['parse']['oper']['save_path']
+    main(ROOT_PATH, SAVE_PATH)
