0625a7f878a79a07a3fd1d3729ca536210bb3d0a
Clarmy Lee <clarmylee92510@gmail.com>
2018-11-08 22:43:23 +0800

    创建init文件

---
+++ algom/__init__.py
@@ -0,0 +1,5 @@
+# coding : utf-8
+from algom.makegrid import full_interp, sd2uv, v_interp, std_sh, \
+                           multi_v_interp
+from algom.io import load_js, parse, parse_data, parse_info, save_as_nc, \
+                     save_as_json
