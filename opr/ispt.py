26dded12eee609cdde18e387e35ac4c112668972
Clarmy Lee <clarmylee92510@gmail.com>
2018-11-23 17:45:43 +0800

    更改了报告的保存方式，直接使用日期作为名称保存；删除了转日判断的功能，对于5s间隔基本无需判断转日；调整和美化了报告的格式；

--- opr/ispt.py
+++ opr/ispt.py
@@ -14,24 +14,26 @@ import os
 import time
 import json as js
 from datetime import datetime
-from opr.optools import check_dir
+from opr.optools import check_dir, get_today_date
 from opr.optools import standard_time_index
 
 with open('../config.json') as f:
     config = js.load(f)
 
 ROOT_PATH = config['parse']['oper']['save_path']
-REPORT_PATH = '/mnt/data14/liwt/opr/parse/inspect/'
+REPORT_PATH = '/mnt/data14/liwt/opr/parse/missing/'
 check_dir(REPORT_PATH)
 
 def report(missing_set,pfn):
     '''生成缺失文件报告'''
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    content = [now+'\n','missing:\n']
+    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    content = ['record\'s utc time:\n\n    {}\n\n'.format(now),
+               'missing files are as follow:\n\n']
     missing_list = list(missing_set)
     missing_list.sort()
     for m in missing_list:
-        content.append(m+'\n')
+        content.append('    {}\n'.format(m))
+    content.append('\n')
 
     with open(pfn,'w') as f:
         f.writelines(content)
@@ -41,34 +43,30 @@ def report(missing_set,pfn):
 
 def main():
     missing_set = set([])
-    pre_folds = os.listdir(ROOT_PATH)
-    pre_folds.sort()
-    last_pre_fold = pre_folds[-1]
     while True:
+
         std_index = standard_time_index()
-        folds = os.listdir(ROOT_PATH)
-        folds.sort()
+        today = get_today_date()
+
+        folds = sorted(os.listdir(ROOT_PATH))
         last_fold = folds[-1]
-        if last_fold == last_pre_fold:
-            path = ROOT_PATH + last_fold + '/'
-            filenames = os.listdir(path)
-            indexs = [fn.split('.')[0] for fn in filenames]
-            indexs.sort()
-            last_time = indexs[-1]
-            dampoint = std_index.index(last_time)
-            if dampoint < len(std_index)-1:
-                std_set = set(std_index[:dampoint+1])
-            else:
-                std_set = set(std_index[:])
-            opr_set = set(indexs)
-            diff_set = std_set - opr_set
-            if diff_set:
-                missing_set.update(diff_set)
-                report(missing_set,REPORT_PATH+'missing.txt')
+        path = ROOT_PATH + last_fold + '/'
+
+        filenames = os.listdir(path)
+        indexs = sorted([fn.split('.')[0] for fn in filenames])
+        last_time = indexs[-1]
+
+        dampoint = std_index.index(last_time)
+        if dampoint < len(std_index)-1:
+            std_set = set(std_index[:dampoint+1])
         else:
-            os.rename(REPORT_PATH+'missing.txt',
-                      REPORT_PATH+'missing'+last_pre_folds+'.txt')
-            last_pre_fold = last_fold
+            std_set = set(std_index)
+        opr_set = set(indexs)
+        diff_set = std_set - opr_set
+        if diff_set:
+            missing_set.update(diff_set)
+            report(missing_set,REPORT_PATH+'%s.txt' % today)
+
         time.sleep(5)
 
 
